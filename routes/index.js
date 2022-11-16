cbdba9d2b53d2aa7b6c08f7f186794633f4eacb6
Angélique <angelique.massolo@gmail.com>
2022-11-16 12:11:58 +0100

    dotenv

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,9 @@ var express = require("express");
 var router = express.Router();
 const fetch = require("node-fetch");
 
-const API = process.env.API_KEY;
 
-console.log(API);
 
-const url = `https://api.themoviedb.org/3/discover/movie?api_key=${API}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&year=2022&with_watch_monetization_types=flatrate`;
+const url = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&year=2022&with_watch_monetization_types=flatrate`;
 
 router.get("/movies", (req, res) => {
   fetch(url)
